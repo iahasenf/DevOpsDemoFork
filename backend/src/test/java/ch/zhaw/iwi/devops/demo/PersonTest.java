e61a03c7774f77422f7d5c5885596963a4d53347
iahasenf <andreas.hasenfratz@bluewin.ch>
2023-06-03 11:55:19 +0200

    Jenkins: New Testcases Person

---
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/PersonTest.java
@@ -0,0 +1,24 @@
+package ch.zhaw.iwi.devops.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PersonTest {
+    @Test
+    public void testSetName() {
+        Person person = new Person();
+        String expectedName = "John Doe";
+
+        person.setName(expectedName);
+
+        Assertions.assertEquals(expectedName, person.getName());
+    }
+
+    @Test
+    public void testEmptyConstructor() {
+        Person person = new Person();
+
+        Assertions.assertEquals(0, person.getId());
+        Assertions.assertEquals(null, person.getName());
+    }
+}
